ade9f399b0d4c7aaf1d4841548af99cea2a05eaf
Aitor <aitor_enrique_gomis_tsapp1ma2021@cev.com>
2022-01-19 17:38:50 +0100

    Realizacion de migraciones, modelos y controllers

---
+++ app/Models/Carta_pertenece.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Carta_pertenece extends Model
+{
+    use HasFactory;
+}
